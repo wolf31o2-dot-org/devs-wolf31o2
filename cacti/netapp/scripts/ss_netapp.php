3a591ec3a6f83f52829a99a6b39022b302c60553
Chris Gianelloni <wolf31o2@gentoo.org>
2008-08-29 16:30:03 -0700

    Move the splitting of the snmp_auth variable to a function, so we can easily re-use it within each of the main functions of the script.

--- cacti/netapp/scripts/ss_netapp.php
+++ cacti/netapp/scripts/ss_netapp.php
@@ -67,30 +67,7 @@ if (!isset($called_by_script_server)) {
 # This is the main function.
 # ============================================================================
 function ss_netapp_luns($hostname, $snmp_auth, $cmd, $arg1 = "", $arg2 = "") {
-	$snmp						= explode(":", $snmp_auth);
-	$snmp_version				= $snmp[0];
-	$snmp_port					= $snmp[1];
-	$snmp_timeout				= $snmp[2];
-
-	$snmp_auth_username			= "";
-	$snmp_auth_password			= "";
-	$snmp_auth_protocol			= "";
-	$snmp_priv_passphrase		= "";
-	$snmp_priv_protocol			= "";
-	$snmp_context				= "";
-	$snmp_community				= "";
-
-	if ($snmp_version == 3) {
-		$snmp_auth_username		= $snmp[4];
-		$snmp_auth_password		= $snmp[5];
-		$snmp_auth_protocol		= $snmp[6];
-		$snmp_priv_passphrase	= $snmp[7];
-		$snmp_priv_protocol		= $snmp[8];
-		$snmp_context			= $snmp[9];
-	} else {
-		$snmp_community			= $snmp[3];
-	}
-
+	ss_netapp_split_snmp $snmp_auth;
 	$baseOID = ".1.3.6.1.4.1.789";
 
 	$lunCount = $baseOID . ".1.17.15.1.0";
@@ -181,6 +158,34 @@ function ss_netapp_luns($hostname, $snmp_auth, $cmd, $arg1 = "", $arg2 = "") {
 		}
 	}
 }
+
+function ss_netapp_split_snmp($snmp_auth) {
+	$snmp						= explode(":", $snmp_auth);
+	$snmp_version				= $snmp[0];
+	$snmp_port					= $snmp[1];
+	$snmp_timeout				= $snmp[2];
+
+	$snmp_auth_username			= "";
+	$snmp_auth_password			= "";
+	$snmp_auth_protocol			= "";
+	$snmp_priv_passphrase		= "";
+	$snmp_priv_protocol			= "";
+	$snmp_context				= "";
+	$snmp_community				= "";
+
+	if ($snmp_version == 3) {
+		$snmp_auth_username		= $snmp[4];
+		$snmp_auth_password		= $snmp[5];
+		$snmp_auth_protocol		= $snmp[6];
+		$snmp_priv_passphrase	= $snmp[7];
+		$snmp_priv_protocol		= $snmp[8];
+		$snmp_context			= $snmp[9];
+	} else {
+		$snmp_community			= $snmp[3];
+	}
+	return $snmp;
+}
+
 
 function ss_netapp_add_high_low($type, $arg, $hostname, $snmp_community, $oids, $snmp_version, $snmp_auth_username, $snmp_auth_password, $snmp_auth_protocol,$snmp_priv_passphrase, $snmp_priv_protocol, $snmp_context, $snmp_port, $snmp_timeout, $arg1 = "") {
 	if ($type == "query") {
